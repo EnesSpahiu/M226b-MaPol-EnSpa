1bd707913359fe3fc58603262f2982e32ef1db82
EnesSpahiu <enes.spahiu@noseryoung.ch>
2022-01-31 14:16:09 +0100

    changed comments

--- Project/src/ch/noseryoung/plj/cherish/cherishes/BirdCherish.java
+++ Project/src/ch/noseryoung/plj/cherish/cherishes/BirdCherish.java
@@ -3,7 +3,8 @@ package ch.noseryoung.plj.cherish.cherishes;
 import ch.noseryoung.plj.cherish.Cherish;
 
 /**
- * This class handles the bird cherisch.
+ * This class handles the bird cherish and is the home of birds.
+ * It extends the cherish class.
  */
 public class BirdCherish extends Cherish {
 }
--- Project/src/ch/noseryoung/plj/cherish/cherishes/FrogCherish.java
+++ Project/src/ch/noseryoung/plj/cherish/cherishes/FrogCherish.java
@@ -3,7 +3,8 @@ package ch.noseryoung.plj.cherish.cherishes;
 import ch.noseryoung.plj.cherish.Cherish;
 
 /**
- * This class handles the frog cherish.
+ * This class handles the frog cherish and is the home of frogs.
+ * It extends the cherish class.
  */
 public class FrogCherish extends Cherish {
 }
